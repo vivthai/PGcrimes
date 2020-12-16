217113b33d2ffec20f5416f70eb4e76d4eb8e5f3
yhuang6540 <71337730+yhuang6540@users.noreply.github.com>
2020-12-16 02:00:20 -0500

    search test

--- js/script.js
+++ js/script.js
@@ -1,11 +1,11 @@
 let busRouteURL = "https://api.umd.io/v0/bus/routes"
 
-    fetch(busRouteURL)
-      .then((response) => {
+fetch(busRouteURL)
+    .then((response) => {
         return response.json();
-      })
-      .then((routes) => {
-          console.log(routes.length);
+    })
+    .then((routes) => {
+        console.log(routes.length);
 
         // loop through all json object
         //   for (i = 0; i < routes.length; i++){
@@ -33,8 +33,52 @@ let busRouteURL = "https://api.umd.io/v0/bus/routes"
         // sessionStorage.setItem("lon_max", route.lon_max);
         // sessionStorage.setItem("lon_min", route.lon_min);
 
-      })
-      .catch((err) => {
+    })
+    .catch((err) => {
         console.log(err);
         main.innerHTML = "Invalid bus route";
-      });
+    });
+
+/********** SEARCH ************* */
+
+const routeArray = [];
+
+fetch(busRouteURL)
+    .then(blob => blob.json())
+    .then(data => routeArray.push(...data))
+
+function findMatches(wordToMatch, arr) {
+    return arr.filter(item => {
+        const regex = new RegExp(wordToMatch, 'gi');
+        return item.title.match(regex)
+
+
+    });
+}
+
+function displayMatches(){
+    //if(document.querySelector('.searchTask').value != ""){
+    suggestions.innerHTML = "<tr><th>Route ID</th><th>Title</th></tr>";
+    const matchArray = findMatches(this.value, routeArray);
+    const html = matchArray.map(mission =>{
+      //const regex = new RegExp(this.value, 'gi');
+      //const missionName = mission.taskName.replace(regex, `<span class="highlight">${this.value}</span>`);
+      return `
+        <tr>
+            <td>${mission.route_id}</td>
+            <td>${mission.title}</td>
+        </tr>
+      `;
+    }).join('');
+    suggestions.innerHTML += html;
+    }
+    //else{
+     // suggestions.innerHTML = "";
+    //}
+  //}
+  
+  const searchInput = document.querySelector('.searchTask');
+  const suggestions = document.querySelector('.suggestions');
+  
+  searchInput.addEventListener('change', displayMatches);
+  searchInput.addEventListener('keyup', displayMatches);
