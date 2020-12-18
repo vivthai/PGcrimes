decebc972f1cd569b9d3365f31e8ef8effed00c6
yhuang6540 <71337730+yhuang6540@users.noreply.github.com>
2020-12-18 16:43:37 -0500

    code cleanup

--- src/routes/results.js
+++ src/routes/results.js
@@ -4,25 +4,24 @@ let db = require('../database');
 
 //GET all results
 resultsRouter.get('/allResults', (req, res) => {
-    
-    console.log("Get all results");
-    let sql = "select * from results";
-    db.all(sql,  (err, row) => {
-        if (err) {
-          res.status(400).json({"error":err.message});
-          return;
-        }
-        res.json({
-            "message":"success",
-            "data":row
-        })
-      });
+  console.log("Get all results");
+  let sql = "select * from results";
+  db.all(sql, (err, row) => {
+    if (err) {
+      res.status(400).json({ "error": err.message });
+      return;
+    }
+    res.json({
+      "message": "success",
+      "data": row
+    })
+  });
 })
 
-//GET a result
+//GET a result with crimeType
 resultsRouter.get('/result', (req, res) => {
   if (!req.query.crimeType) {
-      return res.status(400).send('Missing URL parameter crimeType')
+    return res.status(400).send('Missing URL parameter crimeType')
   }
   let sql = "select results.id, crimeType, searchNum" +
     " from results" +
@@ -30,55 +29,52 @@ resultsRouter.get('/result', (req, res) => {
   console.log("req.query.crimeType: " + req.query.crimeType)
   let params = [req.query.crimeType]
   db.get(sql, params, (err, row) => {
+    if (err) {
+      res.status(400).json({ "error": err.message });
+      return;
+    }
+    res.json({
+      "message": "success",
+      "data": row
+    })
+  });
+})
+
+//PUT update a result
+resultsRouter.put('/result', (req, res) => {
+  console.log("PUT called")
+  var data = {
+    id: req.query.typeId,
+    crimeType: req.body.crimeType,
+    searchNum: req.body.searchNum + 1
+  }
+  console.log("data.id:" + data.id + " crimeType:" + data.crimeType + " searchNum:" + data.searchNum)
+  if (!data.id) {
+    return res.status(400).send('Missing URL parameter id')
+  }
+  db.run(
+    `UPDATE results set 
+         crimeType = ?,
+         searchNum = ?
+         WHERE id = ?`,
+    [data.crimeType, data.searchNum, data.id],
+    function (err, result) {
       if (err) {
-        res.status(400).json({"error":err.message});
+        res.status(400).json({ "error": res.message })
+        console.log(err);
         return;
       }
       res.json({
-          "message":"success",
-          "data":row
+        message: "success",
+        data: data,
+        changes: this.changes
+
       })
     });
 })
 
-//Update
-resultsRouter.put('/result', (req, res) => {
-  console.log("PUT called")
-  var data = {
-      id : req.query.typeId, // **Note that I'm pulling this from the 'query' vs the 'body'
-      crimeType: req.body.crimeType,
-      searchNum:  req.body.searchNum + 1
-  }
-  console.log("data.id:" + data.id + " crimeType:" + data.crimeType + " searchNum:" + data.searchNum)
-  if (!data.id) {
-      return res.status(400).send('Missing URL parameter id')
-  }
-  db.run(
-      `UPDATE results set 
-         crimeType = ?,
-         searchNum = ?
-         WHERE id = ?`,
-      [data.crimeType, data.searchNum, data.id],
-      function (err, result) {
-          if (err){
-              res.status(400).json({"error": res.message})
-              console.log(err);
-              return;
-          }
-          res.json({
-              message: "success",
-              data: data,
-              changes: this.changes
-              
-          })
-  });
-})
-
-
-
-// POST add new crimeType
+// POST add a new result with crimeType
 resultsRouter.post('/result', async (req, res) => {
-  // now we have access to req.body due to body-parser (see index.js)
   if (!req.body) {
     return resizeBy.status(400).send('Request body is missing')
   }
@@ -86,36 +82,35 @@ resultsRouter.post('/result', async (req, res) => {
   let newType = {
     crimeType: req.body.crimeType
   }
-
-  // First make sure that a record doesn't already exist
+  // Check if the crime type is already exist
   let checkIfExistSql = "select * from results where crimeType = ?"
   console.log("req.body.crimeType: " + req.body.crimeType)
   let params = [req.body.crimeType]
   let nameExists = false;
   const rows = await new Promise((resolve, reject) => {
-      db.all(checkIfExistSql, params, (err, rows) => {
-        if (err) {
-          res.status(400).json({ "error": err.message }).send();
-          return;
-        }
-        console.log("ROWS:" + rows.length);
-        if(rows.length > 0){
-          nameExists = true;
-          res.json({
-            "message": "crimeType already exists" 
-          }).send()
-          resolve(rows);
-        }
-        else{
-          resolve(rows);
-        }
-      });
+    db.all(checkIfExistSql, params, (err, rows) => {
+      if (err) {
+        res.status(400).json({ "error": err.message }).send();
+        return;
+      }
+      console.log("ROWS:" + rows.length);
+      if (rows.length > 0) {
+        nameExists = true;
+        res.json({
+          "message": "crimeType already exists"
+        }).send()
+        resolve(rows);
+      }
+      else {
+        resolve(rows);
+      }
     });
+  });
 
-  console.log("crimeType exists:"+ nameExists);
-  if(nameExists)
+  console.log("crimeType exists:" + nameExists);
+  if (nameExists)
     return;
-  
+
   var insertsql = 'INSERT INTO results (crimeType, searchNum) VALUES (?,?)'
   let input = [req.body.crimeType, 1]
   db.run(insertsql, input, function (err, result) {
--- src/server.js
+++ src/server.js
@@ -7,8 +7,6 @@ let db = require("./database.js")
 // Declare filesystem
 let fs        = require('fs');
 
-//The body-parser middleware module, added to the Express.js app, will try to parse the body content 
-//       (URL encoded or JSON) of the post request and store it in req.body object.
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json())
   
@@ -17,4 +15,4 @@ app.use(resultsRoute)
 app.use(express.static('public'))
 const PORT = process.env.PORT || 8080
 
-app.listen(PORT, () => console.info(`Server has started on ${PORT}`)) // Back ticks so I can add a variable
+app.listen(PORT, () => console.info(`Server has started on ${PORT}`)) 
